0d763770117f49ff23716b82de92b7e22b0b5566
Bryan Kho <bryank1991@hotmail.com>
2021-08-08 10:34:20 +0800

    Add embed in discord

--- src/io/discord/webhook.ts
+++ src/io/discord/webhook.ts
@@ -13,18 +13,21 @@ export namespace Discord.Webhook {
     }
     export async function sendMessage(webhookId: string, message: string) {
 
-        let webhook = await getWebhook(webhookId)
-        const webhookClient = await new WebhookClient({ id: "873504876641021992", token: "ELl5D8Xk3Lya5qVojcDmI04P1AHygs3h_mcGdEz4MRSNfwNnKXcUrGzjAlBlfh3CaWQC" });
+        let config = await getWebhook(webhookId)
+        const webhookClient = await new WebhookClient(config.webhook);
+
+        let embed = new MessageEmbed()
+
+        config.title && embed.setTitle(config.title)
+        config.footer && embed.setFooter(config.footer)
+        config.url && embed.setURL(config.url).addFields({ name: "Source", value: config.url })
 
         try {
-            const embed = new MessageEmbed()
-                .setTitle('Some Title')
-                .setColor('#0099ff');
+            embed.setDescription(message + "\n" + message + "\n" + message)
+                .setTimestamp()
 
             webhookClient.send({
-                content: 'Webhook test',
-                username: 'some-username',
-                avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+                username: webhookId,
                 embeds: [embed],
             });
         } catch (e) {
