20267996b0fadbfff57c6fdd6912fa91ab9219af
Bryan Kho <bryank1991@hotmail.com>
2021-10-05 20:52:21 +0800

    Move nvm to later in the init shell

--- init.sh
+++ init.sh
@@ -6,11 +6,6 @@ sudo apt update
 # Install node
 sudo apt install nodejs npm
 
-### Update node to the latest version
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
 # Install chromium depedencies
 sudo apt-get install -yq --no-install-recommends libasound2 libatk1.0-0 libc6 libcairo2 \
 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 \
@@ -20,6 +15,11 @@ libxrender1 libxss1 libxtst6 libnss3 libgbm-dev
 
 # Install chromium
 sudo apt install chromium-browser
+
+### Update node to the latest version
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
 # Install all required npm depedencies
 npm install
