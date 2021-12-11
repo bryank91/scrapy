28987a2eedc31ae2963c73437c60f9b4254efd2e
kronenbourg <ignatiusthien93@hotmail.com>
2021-12-12 03:02:32 +0800

    test

--- src/io/commands/commands.ts
+++ src/io/commands/commands.ts
@@ -16,6 +16,7 @@ export namespace Parse {
   };
 
   const setForever = (doForever: number, func: Function) => {
+    const blah: any = "lol";
     if (doForever >= 3) {
       // sets a hard limit
       console.log("Running forever function...");
