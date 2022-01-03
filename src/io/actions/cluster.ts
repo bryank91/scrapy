c9fbba27dfa90d3f9d3e5e81f074742f641c50b0
Bryan Kho <bryank1991@hotmail.com>
2022-01-03 12:52:16 +0800

    Remove unused functions

--- src/io/actions/cluster.ts
+++ src/io/actions/cluster.ts
@@ -11,35 +11,6 @@ const { addExtra } = require("puppeteer-extra");
 const Stealth = require("puppeteer-extra-plugin-stealth");
 
 export namespace PuppeteerCluster {
-  export async function verifyFunction() {
-    (async () => {
-      const cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 2,
-      });
-
-      await cluster.task(async ({ page, data: url }) => {
-        await page.goto(url);
-        await page.setExtraHTTPHeaders(Config.headers);
-
-        await page.waitForSelector("#AddToCartText-product-template");
-        const result = await page.$$eval("#AddToCartText-product-template", (el) => {
-          return el[0].textContent;
-        });
-        console.log(result);
-      });
-
-      // cluster.queue('https://dailyclack.com/products/gmk-dracula-v2');
-      // cluster.queue('https://dailyclack.com/collections/products/products/durock-red-back-linear-switches');
-      // cluster.queue('https://dailyclack.com/products/gmk-dracula-v2');
-      // cluster.queue('https://dailyclack.com/products/gmk-dracula-v2');
-      cluster.queue("https://dailyclack.com/products/gmk-dracula-v2");
-      // many more pages
-
-      await cluster.idle();
-      await cluster.close();
-    })();
-  }
 
   // future implementation will include our own proxy services
   // let proxy_server = "proxy.soax.com:9000";
