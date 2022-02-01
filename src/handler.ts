1d21ed2b9102bf1e08239deb2188625cd5744200
Bryan Kho <bryank1991@hotmail.com>
2022-02-01 23:52:02 +0800

    Remove depedency for serverless as not used

--- src/handler.ts
+++
@@ -1,8 +0,0 @@
-import { Server } from "./io/server/server";
-const sls = require("serverless-http");
-
-const express = require("express");
-const init = express();
-const server = new Server(init);
-const app = server.run();
-module.exports.server = sls(app);
