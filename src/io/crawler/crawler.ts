d188286596322d3941e6053fd1993031fdec96f9
Bryan Kho <bryank1991@hotmail.com>
2022-04-02 22:19:31 +0800

    Preliminary testing with monitors with node-crawler

--- src/io/crawler/crawler.ts
+++ src/io/crawler/crawler.ts
@@ -1,6 +1,7 @@
 // Crawler atm doesnt have native typescript supportability
 
 import { Data as Config } from "../../data/config";
+
 export namespace Crawler { 
 
     function getConfig(): Config.Crawler {
@@ -17,14 +18,14 @@ export namespace Crawler {
     export function setup() {
         const Crawler = require('crawler');
         const config = getConfig();
+        console.log(config)
         const c = new Crawler({
-            maxConnections: 10, // TODO: fix
+            maxConnections: config.maxConnections,
             callback: (error: any, res : any, done : any) => {
                 if (error) {
                     console.log(error)
                     //TODO: log to discord potentially or another logging method
                 } else {
-                    const $ = res.$;
                     // $ is Cheerio by default
                     //a lean implementation of core jQuery designed specifically for the server
                     console.log(res.$('title').text());
@@ -40,14 +41,21 @@ export namespace Crawler {
         c.queue([{
             uri: profile.url,
             jquery: true,
+            headers: Config.headers,
+            incomingEncoding: 'utf-8',
             
             // global callback will be ignored
             callback: (error:any, res:any, done:any) => {
                 if (error) {
                     console.log(error)
                 } else {
-                    const item = res.$(profile.selector);
-                    console.log(item)
+                    let items : string[] = []
+                    res.$(profile.selector).each((i : number, row : any) => {
+                        items[i] = row.children[0].data;
+                    });
+
+                    console.log(items)
+                    
                 }
             }
         }])
